0cbe59f57717e6a3e9e82d82c6a85fd1b9f5df6f
Shadow15510 <antoine.royer14@orange.fr>
2023-03-08 23:30:16 +0100

    Ré-organisation du dépôt et du code

---
+++ code/vicsek_tests.py
@@ -0,0 +1,36 @@
+import vicsek as vi
+
+def op():
+    order_p = []
+    for noise in np.arange(0, 5, 0.5):
+        op_temp = 0
+        for _ in range(5):
+            grp = vi.group_generator(40, position=(-1.5, 1.5), speed=(-1, 1), noise=(noise, noise), length=3.1)
+            grp.run(200, check_field=False, check_wall=False, dt=0.25)
+            op_temp += grp.order_parameter()
+        order_p.append(op_temp / 5)
+        print()
+    return list(np.arange(0, 5, 0.5)), order_p
+
+
+
+def test():
+    group_1 = vi.group_generator(50, noise=(0, 0), fear=(0, 0))
+    for _ in range(2):
+        group_1.add_agent(vi.agent_generator(speed=(-3, 3), noise=0.25, agent_type=1))
+
+    group_2 = group_1.copy()
+    group_3 = group_1.copy()
+    group_4 = group_1.copy()
+    for i in range(50):
+        group_1[i].noise, group_1[i].fear = 1, 0
+        group_2[i].noise, group_2[i].fear = 0, 1
+        group_3[i].noise, group_3[i].fear = 1, 1
+        group_4[i].noise, group_4[i].fear = 0, 0
+
+    group_1.run(500)
+    group_2.run(500)
+    group_3.run(500)
+    group_4.run(500)
+
+    return len(group_1.dead_agents), len(group_2.dead_agents), len(group_3.dead_agents), len(group_4.dead_agents)
