efa119b83288660d471e64c77ed8ffbd94a605b3
Shadow15510 <antoine.royer14@orange.fr>
2023-05-08 11:26:26 +0200

    Ajout du code d'utilisation dans le rapport et des références à la biblio

--- src/vicsek_stat.py
+++ src/vicsek_stat.py
@@ -1,10 +1,31 @@
-import matplotlib.pyplot as plt
+"""
+Vicsek stat
+===========
+
+Description
+-----------
+Permet de faire des statistiques à partir du modèle de Vicsek et avec différents paramètres.
+"""
 import numpy as np
 import vicsek as vi
 
 
 def op_noise(check_field: bool=False, check_wall: bool=False):
-    """Calcule le paramètre d'alignement pour différentes valeurs de bruits et renvoie un tuple de la forme (bruit, paramètre d'alignement)."""
+    """
+    Calcule le paramètre d'alignement pour différentes valeurs de bruits et renvoie un tuple de la forme (bruit, paramètre d'alignement).
+
+    Paramètres
+    ----------
+    check_field : bool, optionnel
+        Prise en compte de l'angle de vue des agents.
+    check_wall : bool, optionnel
+        Prise en compte des murs.
+
+    Signature
+    ---------
+    out : tuple
+        Tuple de deux listes contenant respectivement les valeurs de bruit et du paramètres d'alignement. 
+    """
     order_p = []
     noises = np.arange(0, 5.5, 0.10)
     for noise in noises:
@@ -13,27 +34,26 @@ def op_noise(check_field: bool=False, check_wall: bool=False):
         grp = vi.group_generator(40, position=(-1.5, 1.5), speed=(-1, 1), noise=(noise, noise), length=3.1)
         grp.run(100, check_field=check_field, check_wall=check_wall, step=0.5)
         op_temp += grp.order_parameter()
-        
+
         order_p.append(op_temp)
         print()
     return list(noises), order_p
 
-A=op_noise()
-plt.figure()
-plt.plot(A[0],A[1], label="N=40")
-plt.xlabel("Bruit")
-plt.ylabel("Paramètre d'alignement")
-plt.legend()
-plt.title("Paramètre d'alignement en fonction du bruit")
-
 
 def op_density():
-    """Calcule le paramètre d'alignement pour différentes densités et renvoie un tuple de la forme (densité, paramètre d'alignement)."""
+    """
+    Calcule le paramètre d'alignement pour différentes densités et renvoie un tuple de la forme (densité, paramètre d'alignement).
+
+    Signature
+    ---------
+    out : tuple
+        Tuple de deux listes contenant respectivement les valeurs de densité et du paramètres d'alignement.
+    """
     order_p = []
-    density = [] 
+    density = []
     grp = vi.group_generator(2, position=(-0.5, 0.5), speed=(-0.5, 0.5), noise=(1.5, 1.5), length=1)
-    
-    for nb in range(100):
+
+    for _ in range(100):
         for _ in range(1):
             agent = vi.agent_generator(position=(-0.5, 0.5), speed=(-0.5, 0.5), noise=(1.5, 1.5))
             grp.add_agent(agent)
@@ -47,15 +67,21 @@ def op_density():
 
 
 def neutral_alignment():
-    """Retourne le paramètre d'alignement en fonction de la densité sans itérer le modèle."""
+    """
+    Retourne le paramètre d'alignement en fonction de la densité sans itérer le modèle.
+
+    Signature
+    ---------
+    out : tuple
+        Tuple de deux listes contenant respectivement les valeurs de densité et du paramètres d'alignement. """
     def get_op():
         order_p = []
-        density = [] 
-        for nb in np.arange(5, 100, 5):
+        density = []
+        for numb in np.arange(5, 100, 5):
             op_temp = 0
             density_temp = 0
             for _ in range(5):
-                grp = vi.group_generator(nb, position=(-10, 10), speed=(-1, 1), noise=(1.5, 1.5), length=20)
+                grp = vi.group_generator(numb, position=(-10, 10), speed=(-1, 1), noise=(1.5, 1.5), length=20)
                 op_temp += grp.order_parameter()
                 density_temp += grp.density
 
@@ -74,12 +100,29 @@ def neutral_alignment():
     return density[0], avg_op
 
 
-def stat(fct, iteration: int=10, *args):
+def stat(fct, *args, iteration: int=10):
+    """
+    Permet de faire des essais sur un plus grand nombre de cas et de moyenner les résultats.
+
+    Paramètres
+    ----------
+    fct : builtin_function_or_method
+        Fonction sur laquelle il faut faire les essais.
+        Cette fonction doit renvoyer un tuple de deux listes.
+    *args : optionnel
+        Arguments de `fct`.
+    iteration : int, optionnel
+            Nobmre de tests à effectuer.
+    Signature
+    ---------
+    out : tuple
+        Tuple de deux listes correspondants aux moyennes des résultats renvoyés par `fct`.
+    """
     runs = []
     for i in range(iteration):
-        print(f"{i+1} / {iteration}") 
+        print(f"{i+1} / {iteration}")
         runs.append(fct(*args))
-    
+
     runs = np.array(runs)
     x_axis = runs[:, 0][0]
     y_axis = runs[:, 1]
@@ -91,7 +134,8 @@ def stat(fct, iteration: int=10, *args):
     return x_axis, avg_y
 
 
-def test():
+def predation():
+    """Calcule le pourcentage de survivant dans des cas extrêmes de bruit et de sensibilité avec 500 itérations."""
     group_1 = vi.group_generator(50, noise=(0, 0), fear=(0, 0))
     for _ in range(2):
         group_1.add_agent(vi.agent_generator(speed=(-3, 3), noise=(0.25, 0.25), agent_type=1))
@@ -113,26 +157,20 @@ def test():
     return len(group_1.dead_agents) / 50, len(group_2.dead_agents) / 50, len(group_3.dead_agents) / 50, len(group_4.dead_agents) / 50
 
 
-def test2():
+def predation_stat():
+    """Statistiques sur 10 lancement de `predation`."""
     rslt = [-1, -1, -1, -1]
     for i in range(10):
         print(f"{i + 1}/10")
-        grps = test()
+        grps = predation()
         for i in range(4):
-            if rslt[i] != -1: rslt[i] = (rslt[i] + grps[i]) / 2
-            else: rslt[i] = grps[i]
+            if rslt[i] != -1:
+                rslt[i] = (rslt[i] + grps[i]) / 2
+            else:
+                rslt[i] = grps[i]
 
     print("noise | fear | % survivants")
     print("1     | 0    |", round(1 - rslt[0], 2))
     print("0     | 1    |", round(1 - rslt[1], 2))
     print("1     | 1    |", round(1 - rslt[2], 2))
     print("0     | 0    |", round(1 - rslt[3], 2))
-
-
-def run():
-    x_without, y_without = stat(op_noise, 10, False, False)
-    x_with, y_with = stat(op_noise, 10, True, False)
-
-    plt.plot(x_without, y_without, label="Sans le cône de vision")
-    plt.plot(x_with, y_with, label="Avec le cône de vision")
-    plt.show()
